3cc65e195b4623f473f40756037c630abb6f9fe4
Svyatoslav Zakhryapin <svt.zhr@gmail.com>
2017-03-02 00:28:05 +0300

    Миграция на гит (#1)

---
+++ GraphLabs.Tasks.Template/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Шаблон задания")]
+[assembly: AssemblyDescription("Ключевые слова, через запятую")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraphLabs")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyVersion("1.0.0.*")]
+#else
+[assembly: AssemblyVersion("1.0.0")]
+#endif
